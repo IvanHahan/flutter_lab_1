3090f6b54946e96d769d0cb80afff5a2972b05cb
Ivan Hahanov <ivanhahanov@gmail.com>
2024-12-10 11:44:50 +0200

    revert lato

--- lib/main.dart
+++ lib/main.dart
@@ -4,18 +4,19 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter_lab_1/screens/tabs.dart';
 
-
 final theme = ThemeData(
   useMaterial3: true,
   colorScheme: ColorScheme.fromSeed(
     brightness: Brightness.dark,
     seedColor: const Color.fromARGB(255, 0, 100, 131),
   ),
-  // textTheme: GoogleFonts.latoTextTheme(),
+  textTheme: GoogleFonts.latoTextTheme(),
 );
 
 void main() {
-  runApp(const ProviderScope(child: App(),));
+  runApp(const ProviderScope(
+    child: App(),
+  ));
 }
 
 class App extends StatelessWidget {
